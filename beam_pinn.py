03e4f852aa5e20e8d649a886658353cbc42f9f82
duck_wad <nickyli13@outlook.com>
2025-06-06 01:22:40 -0400

    pde loss

--- beam_pinn.py
+++ beam_pinn.py
@@ -3,50 +3,23 @@ import pandas as pd
 import os
 import torch
 import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import KFold
 
 # configure the device for GPU if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-''' BEAM PARAMETERS '''
+''' ----------------------- BEAM PARAMETERS ----------------------- '''
 
 L = 10.0 # meters
 E = 210000000000 # Pa
 I = 0.0005 # m4
+tol = 1e-5
 # simply supported
 
 
-''' DEFINE PINN CLASS '''
-
-class BeamPINN(nn.Module):
-    def __init__(self, num_input, num_output, num_neurons, num_layers):
-        super().__init__()
-        activation = nn.Tanh
-        # define the input layer
-        self.input_layer = nn.Sequential(nn.Linear(num_input, num_neurons), activation())
-        # define the hidden layer as sequence of num_layers
-        self.hidden_layer = nn.Sequential(*[nn.Sequential(nn.Linear(num_neurons, num_neurons), activation()) for _ in range(num_layers-1)])
-        # define the output layer with no activation
-        self.output_layer = nn.Linear(num_neurons, num_output)
-    def forward(self, x, w):
-        # torch.cat expects arrays of (N, 1), unsqueeze to get in that form
-        temp = torch.cat([x.unsqueeze(1),w[3].unsqueeze(1)],dim=1)
-        temp = self.input_layer(temp)
-        temp = self.hidden_layer(temp)
-        temp = self.output_layer(temp)
-        return temp
-    def PDE_loss():
-        pass
-
-    def boundary_loss():
-        pass
-
-    def data_loss():
-        pass
-
-    
-''' DEFINE NETWORK '''
+''' ----------------------- NETWORK PARAMETERS ----------------------- '''
 
 # 2 inputs, x and w
 num_input = 2
@@ -57,17 +30,13 @@ num_neurons = 512
 num_layers = 6
 # define the hyperparameters
 learning_rate = 1e-3
-epochs = 1000
+epochs = 50
 lambda_1 = 1e-1 # balance term for boundary condition
 lambda_2 = 1e-4
-
-# define network
-pinn = BeamPINN(num_input, num_output, num_neurons, num_layers).to(device)
-# define optimizer
-optimizer = torch.optim.Adam(pinn.parameters(), lr=learning_rate)
+batch_size = 1024
 
 
-''' PREPARE DATA '''
+''' ----------------------- PREPARE DATA ----------------------- '''
 
 path = "./data"
 files = os.listdir(path)
@@ -92,7 +61,10 @@ for i, file in enumerate(files):
     load = float(file.split(".csv")[0])
     w.append(load)
 
-x = np.array(x) # n_files x n_points 
+# normalize x by beam length to better generalization
+# but need to accomodate for normalization in PDE
+# x = np.array(x) / L # n_files x n_points 
+x = np.array(x)
 y = np.array(y) # n_files x n_points
 # w is not sorted because of sorting standard in file explorer, sort by ascending
 w = np.sort(np.array(w)) # n_files
@@ -121,7 +93,62 @@ Y_all = Y_all[train_index]
 kf = KFold(n_splits=5, shuffle=True, random_state=69)
 
 
-''' DEFINE TRAINING LOOP '''
+''' ----------------------- PINN CLASS ----------------------- '''
+
+class BeamPINN(nn.Module):
+    def __init__(self, num_input, num_output, num_neurons, num_layers):
+        super().__init__()
+        activation = nn.Tanh
+        # define the input layer
+        self.input_layer = nn.Sequential(nn.Linear(num_input, num_neurons), activation())
+        # define the hidden layer as sequence of num_layers
+        self.hidden_layer = nn.Sequential(*[nn.Sequential(nn.Linear(num_neurons, num_neurons), 
+                                                          activation()) for _ in range(num_layers-1)])
+        # define the output layer with no activation
+        self.output_layer = nn.Linear(num_neurons, num_output)
+
+    def forward(self, X_batch):
+        # torch.cat expects arrays of (N, 1), unsqueeze to get in that form
+        #temp = torch.cat([x.unsqueeze(1),w[3].unsqueeze(1)],dim=1)
+        temp = self.input_layer(X_batch)
+        temp = self.hidden_layer(temp)
+        temp = self.output_layer(temp)
+        return temp
+    
+    def PDE_loss(self, Y_domain, X_domain):
+
+        # need to do derivatives wrt the original X_domain tensor rather than splicing X_domain to get the 
+        # x position tensor, that creates a new tensor and grad won't work
+        du = torch.autograd.grad(Y_domain, X_domain, torch.ones_like(Y_domain), create_graph=True)[0]
+        d2u = torch.autograd.grad(du, X_domain, torch.ones_like(du), create_graph=True)[0]
+        d3u = torch.autograd.grad(d2u, X_domain, torch.ones_like(d2u), create_graph=True)[0]
+        d4u = torch.autograd.grad(d3u, X_domain, torch.ones_like(d3u), create_graph=True)[0]
+
+        d4u_dx4 = d4u[:,0].unsqueeze(1)
+
+        # loss is mean squared error
+        # multiply RHS by L^4 to account for the length normalization (x is scaled by 1/L)
+        # return torch.mean((E*I*d4u_dx4 - X_domain[:,1].unsqueeze(1)*L**4)**2)
+        return torch.mean((E*I*d4u_dx4 - X_domain[:,1].unsqueeze(1))**2)
+
+    def boundary_loss(self, Y_bc, X_bc):
+        pass
+
+    def data_loss():
+        pass
+    def compute_loss(self, Y_domain, Y_bc, X_domain, X_bc):
+        loss_PDE = self.PDE_loss(Y_domain, X_domain)
+        print(loss_PDE)
+        loss = lambda_2*loss_PDE
+        return loss
+        # call the other loss functions and sum
+        
+
+
+''' ----------------------- TRAINING LOOP ----------------------- '''
+
+# for each of k folds, iterate for {epoch} epochs
+# in each epoch
 
 for fold, (train_index, valid_index) in enumerate(kf.split(X_all)):
     print(f"Fold {fold + 1}")
@@ -132,8 +159,49 @@ for fold, (train_index, valid_index) in enumerate(kf.split(X_all)):
     X_valid = (X_all[valid_index]).reshape(-1,2)
     Y_valid = (Y_all[valid_index]).reshape(-1,1)
     # convert to torch tensors
-    X_train = torch.tensor(X_train, requires_grad=True).to(device)
-    Y_train = torch.tesnor(Y_train, requires_grad=True).to(device)
-    X_valid = torch.tensor(X_valid, requires_grad=True).to(device)
-    Y_valid = torch.tensor(Y_valid, requires_grad=True).to(device)
+    X_train = torch.tensor(X_train, requires_grad=True, dtype=torch.float32).to(device)
+    Y_train = torch.tensor(Y_train, requires_grad=True, dtype=torch.float32).to(device)
+    X_valid = torch.tensor(X_valid, requires_grad=True, dtype=torch.float32).to(device)
+    Y_valid = torch.tensor(Y_valid, requires_grad=True, dtype=torch.float32).to(device)
+
+    # batch the training data into batches of {batch_size}
+    train_dataset = TensorDataset(X_train, Y_train)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    valid_dataset = TensorDataset(X_valid, Y_valid)
+    valid_dataset = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
     
+    # define network
+    model = BeamPINN(num_input, num_output, num_neurons, num_layers).to(device)
+    # define optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(epochs):
+        for X_batch, Y_batch in train_loader:
+            X_batch = X_batch.to(device)
+            Y_batch = Y_batch.to(device)
+
+            # find the indices of boundary points where x==0 or x==L
+            x_vals = X_batch[:,0].unsqueeze(1)
+            is_left = torch.abs(x_vals - 0.0) < tol
+            is_right =  torch.abs(x_vals - L) < tol
+            is_bc = is_left | is_right
+            is_domain = ~is_bc
+            # split boundary and domain points
+
+            X_domain = X_batch[is_domain.squeeze()].detach().clone().requires_grad_()
+            X_bc = X_batch[is_bc.squeeze()].detach().clone().requires_grad_()
+
+            Y_domain = model(X_domain)
+            Y_bc = model(X_bc)
+
+            # FILL IN
+            loss = model.compute_loss(Y_domain, Y_bc, X_domain, X_bc)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+    exit()
+    # COMPUTE VALIDATION LOSS
+
+            
+
+
