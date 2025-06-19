f1fd96f73deb4c4a28475319a91a5d2b3255da6d
Nick <nickyli13@outlook.com>
2025-06-19 02:11:37 -0400

    run script

---
+++ run_alliance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=0-05:00
+#SBATCH --account=def-rgracie
+#SBATCH --gpus-per-node=v100:1
+#SBATCH --mem=4G
+pip install --no-index --upgrade pip
+pip install --no-index torch
+pip install --no-index scikit-learn
+
+python beam_pinn.py 
