62a0500d70c6252837c12730f73f760ec9dba8c5
Nick <nickyli13@outlook.com>
2025-06-24 01:40:40 -0400

    de

--- run_alliance.sh
+++ run_alliance.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=0-05:00
 #SBATCH --account=def-rgracie
-#SBATCH --gpus-per-node=v100:1
+#SBATCH --gpus-per-node=1
 #SBATCH --mem=4G
 pip install --no-index --upgrade pip
 pip install --no-index torch
